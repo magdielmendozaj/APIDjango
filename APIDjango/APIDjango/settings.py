f93ab65990a2278245152b844706e3443a158a20
magdielmendozaj <2019452087@teschi.edu.mx>
2023-10-27 20:09:56 -0600

    new changes again

--- APIDjango/APIDjango/settings.py
+++ APIDjango/APIDjango/settings.py
@@ -77,27 +77,27 @@ WSGI_APPLICATION = 'APIDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DevProfileHub',
-        'USER': 'postgres',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'devprofilehub',
-#         'USER': 'magdiel123',
-#         'PASSWORD': 'lXj636xFwHPPIYJV9h3J3KppIRkF0FAK',
-#         'HOST': 'oregon-postgres.render.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'DevProfileHub',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin123',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devprofilehub',
+        'USER': 'magdiel123',
+        'PASSWORD': 'lXj636xFwHPPIYJV9h3J3KppIRkF0FAK',
+        'HOST': 'oregon-postgres.render.com',
+        'PORT': '5432',
+    }
+}
 
 # AUTHENTICATIONS
 
